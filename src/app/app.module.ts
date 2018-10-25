54c1fefc766571670f10ad9f4534039f4655d7ed
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-10-25 14:29:35 -0300

    Removendo páginas inuteis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,26 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
-
-import { WiseAuthInterceptor } from '../providers/wiseauth.interceptor';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { AuthProvider } from '../providers/auth/auth';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+
+import { AuthProvider } from '../providers/auth/auth';
 import { RegisterPage } from '../pages/register/register';
 import { BaseHttp } from '../providers/basehttp';
 import { MainToolbarComponent } from '../components/main-toolbar/main-toolbar';
 import { EmptyScheduleCardComponent } from '../components/empty-schedule-card/empty-schedule-card';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { WiseAuthInterceptor } from '../providers/wiseauth.interceptor';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     LoginPage,
     RegisterPage,
@@ -43,8 +38,6 @@ import { EmptyScheduleCardComponent } from '../components/empty-schedule-card/em
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     LoginPage,
     RegisterPage,
