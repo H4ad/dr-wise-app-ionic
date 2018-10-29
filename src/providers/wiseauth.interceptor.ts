d4f82f6e4d7985be2cced8360a238ec13cb93869
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-10-29 14:06:24 -0300

    Arrumando import do rxjs

--- src/providers/wiseauth.interceptor.ts
+++ src/providers/wiseauth.interceptor.ts
@@ -2,7 +2,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 
 //#endregion
 
